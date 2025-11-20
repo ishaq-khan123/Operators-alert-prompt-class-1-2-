92c86721357d0a821d91430aa09e11397d17fc90
ishaq-khan123 <muhammadishaqk401@gmail.com>
2025-11-20 13:24:17 +0500

    alert and prompt all questions added

---
+++ Apps.js
@@ -0,0 +1,60 @@
+console.log("hello world");
+
+//(Question:01)
+// alert('welcome, Good Morning')
+// var username = prompt("What's your name. Please!")
+// console.log('My name is ', username)
+
+//(Question:02)
+// var username = prompt("What's your name. Please!")
+// var age = prompt("What's your age.")
+// console.log('My name is ' + username + ' and I am ' + age + ' years old')
+
+//(Question:03)
+// var color = prompt("What's your favourite color.")
+// alert("Wow " + color +  " is nice color")
+// console.log('My foavourtie color is ' + color)
+//(Question:04)
+// var num1 = prompt("Enter first number")
+// var num2 = prompt("Enter second number")
+// console.log('The result of ' + num1 + '+' + num2 + ' is = ' + (num1 + num2 ) + '.')
+
+//(Question:05)
+// var username = prompt("what's your name. Please!")
+// var hobby = prompt("what's your hobby")
+// var country = prompt("what's your country name")
+// alert("Hi! i'm " + username + " from " + country + " and I like " + hobby + ".")
+// console.log('My name is ' + username + ' I am from ' + country + ' and I like ' + hobby + '.')
+
+//(Question:06)
+// var street = prompt("Enter your street ")
+// var city = prompt("Enter your city name ")
+// var country = prompt("Enter your country name")
+// console.log(' Your full address is : ' + street + ' , ' + city + ' , ' + country + ' . ')
+
+//(Question:07)
+// var firstname = prompt("Enter your first name :")
+// var num = prompt("Enter your favourite number :")
+// console.log('Your new username is :' + firstname + ( num))
+
+//(Question:08)
+// alert("email generator")
+// var firstname = prompt("Enter your firstname")
+// var lastname = prompt("Enter your lastname")
+// console.log( firstname + ( lastname )+'@gmail.com')
+
+//(Question:09)
+// alert("Your Favourite things")
+// var color = prompt("what's your favourite color")
+// var animal = prompt("what's your favourite animal")
+// var food = prompt("what's your favourite food")
+// console.log("Your favourite things are : " +(color+" " ) + (animal+" " ) + (food+"!"))
+
+//(Question:10)
+// alert("Quote and Author's name :")
+// var quote = prompt("Write a Qoute you love :")
+// var authname = prompt("Write Author's name :")
+// console.log( quote + ' _ ' + authname)
+
+
+
