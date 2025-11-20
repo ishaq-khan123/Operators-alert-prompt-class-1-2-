8fcdb7c461cf6537ffc6f757b08b31a24970d1dd
ishaq-khan123 <muhammadishaqk401@gmail.com>
2025-11-20 13:13:31 +0500

    Question 02 added

--- alert&prompt.js
+++ alert&prompt.js
@@ -3,3 +3,11 @@
 // alert('welcome, Good Morning')
 // var username = prompt("What's your name. Please!")
 // console.log('My name is ', username)
+
+//(Question:02)
+// var username = prompt("What's your name. Please!")
+// var age = prompt("What's your age.")
+// console.log('My name is ' + username + ' and I am ' + age + ' years old')
+
+
+
