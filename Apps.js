aec2ae8be14860fb1030ad9742320454807e6228
ishaq-khan123 <muhammadishaqk401@gmail.com>
2025-11-20 13:26:23 +0500

    learned topics added

--- Apps.js
+++ Apps.js
@@ -58,3 +58,83 @@
 
 
 
+
+
+
+///learned topics
+///////////////////////////////////////////////////////////////////
+
+//console.log("")//debuging and for print something in console
+//container//variable//variable aik container banata hain
+//alert//prompt// showing a popup on the browser
+// //concatination
+// a = (20)
+// console.log(a + "10")
+
+////////////// Arithmatic operator
+//+ add
+//- subb
+//(/) divide
+//* multiply
+//% modulus
+// ++a pre increment
+// a++ post increment
+// --a pre decrement
+// a-- post decrement
+
+/////////////Assignment opperators
+//=
+//+=
+//-=
+//*=
+//(/=)
+
+// var x = 50
+// console.log('x =', x)
+// var assign = 50;
+// assign +=10;    //same as assign = assign + 10;
+// console.log('assign value =', assign)
+// assign = 20;
+// assign -= 10;   //same as assign = assign - 10;
+// console.log('value =', assign)
+// assign = 30;
+// assign = 15;
+// assign *= 2;     //same as assign = assign * 10;
+// console.log('value =', assign)
+// assign = 30;
+// assign /= 6;     //same as assign = assign / 10;
+// console.log('value =', assign)
+
+// comparision operator (hamesha boolean return karta ha)
+// >
+// <
+// ==
+// === (data type bhi check krta ha)
+// !=
+// !== x
+// >=
+// <=
+
+// var num1 = 20;
+// var num2 = 10;
+// console.log(num1<num2)
+// num1 = 30;
+// num2 = 20;
+// console.log(num1>num2)
+// num = 40;
+// num = 40;
+// console.log(40 == 40) //true // == is said to be only equal to
+// console.log(40 == "40") //true  // == is said to be only equal to it does not check the data type.
+// num = 13
+// num = 13
+// console.log(13 == "13")   
+// num = 15;
+// num = 15;
+// console.log(15 === "15")  //false  //b/c data type is changed// === this is double equal to(===) but it check the data type.num = 15;
+// num1 = 15;
+// num2 = 15;
+// console.log(num1 === num2)//true  //b/c data type is same// === this is double equal to(===) but it check the data type.num = 15;
+
+
+
+
